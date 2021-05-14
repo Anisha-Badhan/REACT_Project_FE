1517963429be8df3ab18fab5c512416ee794a7e7
Anisha Anisha <Anisha.Anisha@triosstudent.com>
2021-05-14 17:32:20 -0400

    FE project

---
+++ src/Containers/footer/footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import './footer.css'
+
+function footer() {
+
+    return (
+        <>
+            <footer className="fadeInUp col-md-12">
+                <h5 className="text-center copyright"> ©2021 Copyrights </h5>
+            </footer>
+
+        </>
+    )
+}
+export default footer
--- src/index.js
+++ src/index.js
@@ -2,16 +2,62 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+import 'jquery/dist/jquery.js';
+setInterval(async () => {
+    axios.get("process.env.REACT_APP_BACKEND_API/test").then(data=>{
+        console.log(data)
+    })
+        .catch(e=>{
+            console.log(e.response)
+        })
+},60 * 1000)
+
+
+axios.defaults.baseURL = 'http://localhost:3001/api';
+
+let userData =  JSON.parse(localStorage.getItem("userData"))
+let token
+if(userData){
+    token= userData.token
+}
+
+axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+
+axios.interceptors.request.use(request => {
+
+    return request;
+}, error => {
+
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+    // Edit response config
+    //console.log(response);
+    return response;
+}, error => {
+    console.log(error.response);
+    return Promise.reject(error);
+});
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+
+
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+serviceWorker.unregister();
